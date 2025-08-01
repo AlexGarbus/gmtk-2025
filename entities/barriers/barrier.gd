e1e88a0d010a92dad125ca893d075349d944bdc0
AlexGarbus <alex.garbus@outlook.com>
2025-08-01 17:17:14 -0400

    Mesh library updates & barrier animation

--- entities/barriers/barrier.gd
+++ entities/barriers/barrier.gd
@@ -4,18 +4,41 @@ extends StaticBody3D
 @export var _flag := 0
 @export var _invert_flag := false
 
+@onready var _anim: AnimationPlayer = $AnimationPlayer
+@onready var _collision_shape: CollisionShape3D = $CollisionShape3D
+
+var _activated := true
+
 
 func _ready() -> void:
-	if not _is_on(UserData.flags.get_bit(_flag)):
-		Utils.set_node_enabled(self, false)
+	_activated = _is_flag_condition_met(UserData.flags.get_bit(_flag))
+	_collision_shape.set_deferred("disabled", not _activated)
+	_initialize_animation()
 	UserData.flags.changed.connect(_on_flags_changed)
 
 
-func _is_on(flag_value: bool) -> bool:
+func _initialize_animation() -> void:
+	if _activated:
+		_anim.play("toggle")
+	else:
+		_anim.play_backwards("toggle")
+	_anim.advance(0)
+	_anim.pause()
+
+
+func _is_flag_condition_met(flag_value: bool) -> bool:
 	return flag_value and not _invert_flag or not flag_value and _invert_flag
 
 
 func _on_flags_changed(bit: int, value: bool) -> void:
 	if not _flag == bit:
 		return
-	Utils.set_node_enabled(self, _is_on(value))
+	if _activated == _is_flag_condition_met(value):
+		return
+	print("TOGGLE")
+	_activated = not _activated
+	_collision_shape.set_deferred("disabled", not _activated)
+	if _activated:
+		_anim.play_backwards("toggle")
+	else:
+		_anim.play("toggle")
--- globals/utils.gd
+++ globals/utils.gd
@@ -5,4 +5,4 @@ extends RefCounted
 static func set_node_enabled(node: Node3D, value: bool) -> void:
 	node.visible = value
 	var process_mode := Node.PROCESS_MODE_INHERIT if value else Node.PROCESS_MODE_DISABLED
-	node.call_deferred("set_process_mode", process_mode)
+	node.set_deferred("process_mode", process_mode)
